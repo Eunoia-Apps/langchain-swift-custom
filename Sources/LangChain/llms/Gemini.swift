0db56a42987465e8399b7c8e98fcd292bfa533c7
Eba <121199967+ostracizedDucky@users.noreply.github.com>
2024-06-29 11:13:58 -0400

    Update Gemini.swift

--- Sources/LangChain/llms/Gemini.swift
+++ Sources/LangChain/llms/Gemini.swift
@@ -13,7 +13,14 @@ public class Gemini: LLM {
         let env = LC.loadEnv()
         
         if let apiKey = env["GOOGLEAI_API_KEY"] {
-            let model = GenerativeModel(name: "gemini-pro", apiKey: apiKey)
+            let model = GenerativeModel(name: "gemini-1.0-pro-latest", apiKey: apiKey, safetySettings: [
+        
+        SafetySetting(harmCategory: .dangerousContent, threshold: .blockNone),
+        SafetySetting(harmCategory: .harassment, threshold: .blockNone),
+        SafetySetting(harmCategory: .hateSpeech, threshold: .blockNone),
+        SafetySetting(harmCategory: .sexuallyExplicit, threshold: .blockNone),
+        
+    ])
             let response = try await model.generateContent(text)
             return LLMResult(llm_output: response.text)
         } else {
